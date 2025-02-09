7bb7554d70ab0154cf141b8a21b16b62e47628dd
212601662 <shreshth.kandari@wabtec.com>
2025-02-09 03:57:27 -0600

    feat: Add Footer section with copyright information

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import PictureCard from './components/PictureCard'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
+import Footer from './sections/Footer/Footer'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Projects />
       <PictureCard title="Incredible accommodation for your team" desc="Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of
               places to do just that." url="./background.jpg" />
+      <Footer />
     </>
   )
 }
