2e10a1d8fe2eafcf75fded25d928b6fdb962d490
212601662 <shreshth.kandari@wabtec.com>
2025-02-09 03:48:03 -0600

    feat: Add Projects section with responsive project cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import PictureCard from './components/PictureCard'
 import Hero from './sections/Hero/Hero'
-
+import Projects from './sections/Projects/Projects'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Hero />
+      <Projects />
       <PictureCard title="Incredible accommodation for your team" desc="Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of
               places to do just that." url="./background.jpg" />
     </>
